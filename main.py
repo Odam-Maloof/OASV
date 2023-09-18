42a37f2a6535dcf96c86f16fb7b2094d6ea2b8b6
Nicholas Shires <shiresthehobbit@gmail.com>
2023-09-18 21:03:18 +1200

    final changes from trialing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as numpy
 import librosa
 import time
 import threading
-import playsound
+import scipy.signal
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -22,8 +22,10 @@ class AudioAnalyzerThread(threading.Thread):
         super().__init__()
 
     def run(self):
+        
         global selected_audio
         file_path = selected_audio
+        
         global amplitude_values
         interval_ms = interval
         try:
@@ -49,6 +51,45 @@ class AudioAnalyzerThread(threading.Thread):
             end_sample = (i + 1) * interval_samples
             interval_amplitude = numpy.max(numpy.abs(y[start_sample:end_sample]))
             amplitude_values.append(interval_amplitude)
+        '''
+        global selected_audio
+        file_path = selected_audio
+        
+        global amplitude_envelopes
+        interval_ms = interval
+        try:
+            if audio_analyzer_thread and audio_analyzer_thread.is_alive():
+                    return
+        except UnboundLocalError:
+            pass
+        # Load the audio file
+        y, sr = librosa.load(file_path)
+
+        # Calculate the number of samples in each interval
+        interval_samples = int((interval_ms / 1000) * sr)
+
+        # Calculate the number of intervals
+        num_intervals = len(y) // interval_samples
+
+        # Initialize a list to store amplitude envelope values
+        amplitude_envelopes = []
+
+        # Rectify the audio signal (convert negative values to positive)
+        y_rectified = numpy.abs(y)
+
+        # Apply a low-pass filter to smooth the envelope (optional)
+        # Adjust the filter parameters as needed
+        cutoff_frequency = 10.0  # Adjust this value as needed
+        filter_order = 3  # Adjust this value as needed
+        b, a = scipy.signal.butter(filter_order, cutoff_frequency / (0.5 * sr), 'low')
+        y_smoothed = scipy.signal.filtfilt(b, a, y_rectified)
+
+        # Iterate over intervals and calculate the amplitude envelope for each
+        for i in range(num_intervals):
+            start_sample = i * interval_samples
+            end_sample = (i + 1) * interval_samples
+            interval_amplitude = numpy.max(y_smoothed[start_sample:end_sample])
+            amplitude_envelopes.append(interval_amplitude)'''
         
 
 # make labels clickable with this class
@@ -173,7 +214,7 @@ class LoadingBarWidget(QWidget):
 first_run = True
 progress = 0
 paused_position = 0
-interval = 3
+interval = 5
 # create thread
 audio_analyzer_thread = None
 
@@ -416,8 +457,8 @@ def display_details():
 
 def finalise_upload():
     global paused_position, bar_x
-    '''pause_audio()
-    paused_position = 0'''
+    pause_audio()
+    paused_position = 0
     file_name_pathless = os.path.basename(file_name)
     artist, song = get_song_info(file_name_pathless)
     if len(song) > 13:
@@ -472,7 +513,7 @@ def skip_audio():
         paused_position = audio_player.position()
         paused_position = paused_position + 15000
         audio_player.setPosition(paused_position)
-        k = int(paused_position // 3)
+        k = int(paused_position // interval)
     except Exception:
         print('what')
 
@@ -529,8 +570,10 @@ def cancel_loading():
 k = 0
 l = 0
 bar_x = 656
+scaled_height = 0
+tumour = 1
 def update_visualizer():
-    try:
+    '''try:
         global k, l, bar_x
         bar_y = 534
         bar_gap = 32
@@ -548,7 +591,104 @@ def update_visualizer():
             l = 0
             bar_x = 656
     except IndexError:
+        audio_timer.stop()'''
+    '''try:
+        global k, l, bar_x, scaled_height, numer
+        bar_y = 540
+        bar_gap = 32
+        label_list = window.findChildren(QLabel, "bar_")
+        # height_from = label_list[0].height()
+        numer = 0
+        for label in label_list:
+            print(label_list[numer].height())
+            if label == label_list[0]:
+                print('label_1')
+            else:
+                print(numer, ' ', label_list[numer].height())
+                bar_x += bar_gap
+                label.setFixedHeight(label_list[numer].height())
+                bar_y = 540 - (label_list[numer].height()/2)
+                label.move(bar_x, int(bar_y))
+                numer += 1
+        try:            
+            if label_list[l] == label_list[0]:
+                pass
+            else:
+                print(k)
+                # print(numer)
+                print(l)
+                bar_x = label_list[l-1].x() + 32
+                amplitude_data = amplitude_values[k-1]
+                if k-numer < 0:
+                    pass
+                else:
+                    scaled_height = 12 + (amplitude_data * 400)
+                    scaled_height = int(scaled_height)
+                    bar_y = 540 - (scaled_height/2)
+                    label_list[l].move(bar_x, int(bar_y))
+                    label_list[l].setFixedHeight(scaled_height)
+        except Exception as e:
+            print(e)
+            print('what')'''
+    global k, l, bar_x, scaled_height, numer, tumour
+    try:
+        bar_y = 540
+        bar_gap = 32
+        label_list = window.findChildren(QLabel, "bar_")
+        l = 0
+        numer = 0
+        bar_x = 656
+        for i in range(0, tumour):
+            amplitude_data = amplitude_values[k-numer]
+            scaled_height = 12 + (amplitude_data * 400)
+            scaled_height = int(scaled_height)
+            bar_y = 540 - (scaled_height/2)
+            label_list[l].move(bar_x, int(bar_y))
+            label_list[l].setFixedHeight(scaled_height)
+            bar_x += bar_gap
+            l += 1
+            numer += 1
+
+        if tumour != 20:
+            tumour += 1    
+        k += 1
+        if l == 20:
+            l = 0
+            bar_x = 656
+    except IndexError:
+        print('what')
         audio_timer.stop()
+    '''
+    global k, l, bar_x, scaled_height, numer, tumour
+    try:
+        bar_y = 540
+        bar_gap = 32
+        label_list = window.findChildren(QLabel, "bar_")
+        l = 0
+        numer = 0
+        bar_x = 656
+        for i in range(0, tumour):
+            amplitude_data = amplitude_envelopes[k-numer]
+            scaled_height = 12 + (amplitude_data * 400)
+            scaled_height = int(scaled_height)
+            bar_y = 540 - (scaled_height/2)
+            label_list[l].move(bar_x, int(bar_y))
+            label_list[l].setFixedHeight(scaled_height)
+            bar_x += bar_gap
+            l += 1
+            numer += 1
+
+        if tumour != 20:
+            tumour += 1    
+        k += 1
+        if l == 20:
+            l = 0
+            bar_x = 656
+    except IndexError:
+        print('what')
+        audio_timer.stop()'''
+    
+    
 
 
 def get_amplitudes(file_path):
